c07f731e31e828ea460096de192d1d2eff835ed8
veerachot113 <veerachot.ro.64@ubu.ac.th>
2024-06-09 01:49:34 +0700

    aaa

--- rice_harvest_schedule/auth_admin/models.py
+++ rice_harvest_schedule/auth_admin/models.py
@@ -6,7 +6,7 @@ class LicenseDocument(models.Model):
     id_card = models.FileField(upload_to='license_documents/id_card/')
     driving_license = models.FileField(upload_to='license_documents/driving_license/')
     vehicle_registration = models.FileField(upload_to='license_documents/vehicle_registration/')
-    photo = models.ImageField(upload_to='license_documents/photo/')
+    photo = models.ImageField(upload_to='license_documents/photo/', default='')
     is_approved = models.BooleanField(default=False)
     is_rejected = models.BooleanField(default=False)
     reason = models.TextField(blank=True, null=True ,default='')  # เปลี่ยนจาก rejection_reason เป็น reason
